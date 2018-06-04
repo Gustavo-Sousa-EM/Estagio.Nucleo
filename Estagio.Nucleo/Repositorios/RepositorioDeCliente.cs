59d6435b6e75daacb4d857faf7018cc6cdea8996
gusMuller <gustavo.sousa@escolarmanager.com.br>
2018-06-04 08:57:27 -0300

    Correção de erros referente a using

--- Estagio.Nucleo/Repositorios/RepositorioDeCliente.cs
+++ Estagio.Nucleo/Repositorios/RepositorioDeCliente.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Estagio.Nucleo.IRepositorio;
 
 namespace Estagio.Nucleo.Repositorios
 {
--- Estagio.Nucleo/Repositorios/RepositorioDeMovimentacao.cs
+++ Estagio.Nucleo/Repositorios/RepositorioDeMovimentacao.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Estagio.Nucleo.Repositorios
 {
     class RepositorioDeMovimentacao : IRepositorio<MovimentacaoDeEstoqueAbstrato>
